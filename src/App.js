545d121a4923c8c3c88e1649d1947240feec72ec
GeoffA12 <Geoffarroyo@gmail.com>
2020-10-17 03:09:22 -0500

    Adding User Secure Storage so that users are persisted on their device after logging in to our app. Created splash screen and implemented loading state so that the user doesn't have to flash through login and register screeens should they have a valid token.

--- src/App.js
+++ src/App.js
@@ -1,84 +1,45 @@
-import React, { useMemo, useReducer } from 'react';
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import axios from 'axios';
 
 import AuthStackNavigator from './navigators/AuthStackNavigator';
 import MainStackNavigator from './navigators/MainStackNavigator';
+import SplashScreen from './screens/SplashScreen';
 import AuthContext from './contexts/AuthContext';
 import UserContext from './contexts/UserContext';
-import { BASE_URL_GEOFF_LOCAL } from './config/APIConfig';
-import { BASE_URL_DROPLET } from './config/APIConfig';
-import { createAction } from './config/CreateAction';
+import { navigationRoutes } from './config/NavConfig';
+import { useAuth } from './hooks/Auth';
 
 const RootStack = createStackNavigator();
 
 const App = () => {
-    const [state, dispatch] = useReducer((currentState, action) => {
-        switch (action.type) {
-            case 'SET_USER':
-                return {
-                    ...state,
-                    user: { ...action.payload },
-                };
-            case 'REMOVE_USER':
-                return {
-                    ...state,
-                    user: undefined,
-                };
-            default:
-                return currentState;
+    const { auth, state } = useAuth();
+
+    function renderScreens() {
+        if (state && state.loading) {
+            return (
+                <RootStack.Screen
+                    name={navigationRoutes.SPLASHSCREEN}
+                    component={SplashScreen}
+                />
+            );
+        } else {
+            return state && state.user ? (
+                <RootStack.Screen name={'MainStack'}>
+                    {() => (
+                        <UserContext.Provider value={{ user: state.user }}>
+                            <MainStackNavigator />
+                        </UserContext.Provider>
+                    )}
+                </RootStack.Screen>
+            ) : (
+                <RootStack.Screen
+                    name={'AuthStack'}
+                    component={AuthStackNavigator}
+                />
+            );
         }
-    });
-    const auth = useMemo(
-        () => ({
-            login: async (username, password) => {
-                const url = `${BASE_URL_DROPLET}/authenticateUserLogin`;
-                const response = await axios.post(url, {
-                    username,
-                    hashPassword: password,
-                });
-                const { data } = response;
-                if (data) {
-                    const user = {
-                        firstName: data.firstName,
-                        lastName: data.lastName,
-                        username: data.username,
-                        phone: data.phone,
-                        email: data.email,
-                        authToken: data.authToken,
-                    };
-                    dispatch(createAction('SET_USER', user));
-                }
-                return response;
-            },
-            logout: () => {
-                dispatch(createAction('REMOVE_USER'));
-            },
-            register: async (
-                siteRole,
-                firstName,
-                lastName,
-                email,
-                phone,
-                username,
-                password
-            ) => {
-                const url = `${BASE_URL_DROPLET}/createUser`;
-                const response = await axios.post(url, {
-                    siteRole,
-                    firstName,
-                    lastName,
-                    username,
-                    email,
-                    phone,
-                    password,
-                });
-                return response;
-            },
-        }),
-        []
-    );
+    }
 
     return (
         <AuthContext.Provider value={auth}>
@@ -88,21 +49,7 @@ const App = () => {
                         headerShown: false,
                         animationEnabled: false,
                     }}>
-                    {state && state.user ? (
-                        <RootStack.Screen name={'MainStack'}>
-                            {() => (
-                                <UserContext.Provider
-                                    value={{ user: state.user }}>
-                                    <MainStackNavigator />
-                                </UserContext.Provider>
-                            )}
-                        </RootStack.Screen>
-                    ) : (
-                        <RootStack.Screen
-                            name={'AuthStack'}
-                            component={AuthStackNavigator}
-                        />
-                    )}
+                    {renderScreens()}
                 </RootStack.Navigator>
             </NavigationContainer>
         </AuthContext.Provider>
