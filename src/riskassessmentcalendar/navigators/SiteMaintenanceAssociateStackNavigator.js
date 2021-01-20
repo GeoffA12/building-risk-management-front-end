5713694f2a15fc95a561e565966df3388b2e3155
GeoffA12 <Geoffarroyo@gmail.com>
2021-01-19 21:47:45 -0600

    Merging with base branch.

--- src/riskassessmentcalendar/navigators/SiteMaintenanceAssociateStackNavigator.js
+++ src/riskassessmentcalendar/navigators/SiteMaintenanceAssociateStackNavigator.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import { navigationRoutes } from '../../config/NavConfig';
 import CalendarScreen from '../screens/CalendarScreen';
 
 const SiteMaintenanceAssociateStack = createStackNavigator();
+const SiteMaintenanceAssociateDrawer = createDrawerNavigator();
 
 const SiteMaintenanceAssociateStackNavigator = () => {
+    function createSiteMaintenanceAssociateStack() {
+        return (
+            <SiteMaintenanceAssociateStack.Navigator mode={'modal'}>
+                <SiteMaintenanceAssociateStack.Screen
+                    name={navigationRoutes.RISKASSESSMENTCALENDARSCREEN}
+                    component={CalendarScreen}
+                />
+            </SiteMaintenanceAssociateStack.Navigator>
+        );
+    }
     return (
         <SiteMaintenanceAssociateStack.Navigator mode={'modal'}>
             <SiteMaintenanceAssociateStack.Screen
--- src/riskassessmentcalendar/screens/CalendarScreen.js
+++ src/riskassessmentcalendar/screens/CalendarScreen.js
@@ -1,48 +1,78 @@
-import React, {useState, useContext, useEffect} from 'react';
-import {View, Text, TouchableOpacity} from 'react-native';
-import {Agenda} from 'react-native-calendars';
-import {Card} from 'react-native-paper';
+import React, { useState, useContext, useEffect } from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { Agenda } from 'react-native-calendars';
+import { Card } from 'react-native-paper';
 import AuthContext from '../../auth/contexts/AuthContext';
 import { useHeader } from '../hooks/CalendarHeader';
 import { BASE_URL } from '../../config/APIConfig';
 
-const CalendarScreen = ({navigation}) => {
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    agendaItemContainer: {
+        marginRight: 10,
+        marginTop: 17,
+    },
+    agendaItemContentContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+});
+
+const CalendarScreen = ({ navigation }) => {
     const {
         auth: { logout },
         user,
     } = useContext(AuthContext);
     const { setCalendarHeader } = useHeader();
-    const [riskAssessmentScheduleIdsList, setRiskAssessmentScheduleIdsList] = useState([]);
-    const [riskAssessmentScheduleList, setRiskAssessmentScheduleList] = useState([]);
+    const [
+        riskAssessmentScheduleIdsList,
+        setRiskAssessmentScheduleIdsList,
+    ] = useState([]);
+    const [
+        riskAssessmentScheduleList,
+        setRiskAssessmentScheduleList,
+    ] = useState([]);
     const [itemsList, setItemsListForAgenda] = useState([]);
     const today = new Date().toISOString().slice(0, 10);
 
     useEffect(() => {
         setCalendarHeader(navigation, logout);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [navigation, logout]);
 
     useEffect(() => {
-        async function getAssignedRiskAssessmentScheduleIds(){
-            const siteMaintenanceAssociate = await fetch(`${BASE_URL}/getSiteMaintenanceAssociateById?id=${user.id}`);
+        async function getAssignedRiskAssessmentScheduleIds() {
+            const siteMaintenanceAssociate = await fetch(
+                `${BASE_URL}/getSiteMaintenanceAssociateById?id=${user.id}`
+            );
             const data = await siteMaintenanceAssociate.json();
             const idsList = data.assignedRiskAssessmentScheduleIds;
             setRiskAssessmentScheduleIdsList(idsList);
         }
         getAssignedRiskAssessmentScheduleIds();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     useEffect(() => {
-        async function getRiskAssessmentSchedules(){
-            if(riskAssessmentScheduleIdsList.length > 0){
-                const rawData = JSON.stringify({"riskAssessmentScheduleIds": riskAssessmentScheduleIdsList});
-                const riskAssessmentSchedules = await fetch(`${BASE_URL}/getRiskAssessmentSchedulesByRiskAssessmentSchedulesIdsList`, {
-                method: 'POST',
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json'
-                },
-                body: rawData
+        async function getRiskAssessmentSchedules() {
+            if (riskAssessmentScheduleIdsList.length > 0) {
+                const rawData = JSON.stringify({
+                    riskAssessmentScheduleIds: riskAssessmentScheduleIdsList,
                 });
+                const riskAssessmentSchedules = await fetch(
+                    `${BASE_URL}/getRiskAssessmentSchedulesByRiskAssessmentSchedulesIdsList`,
+                    {
+                        method: 'POST',
+                        headers: {
+                            Accept: 'application/json',
+                            'Content-Type': 'application/json',
+                        },
+                        body: rawData,
+                    }
+                );
                 const data = await riskAssessmentSchedules.json();
                 setRiskAssessmentScheduleList(data);
             }
@@ -51,10 +81,12 @@ const CalendarScreen = ({navigation}) => {
     }, [riskAssessmentScheduleIdsList]);
 
     useEffect(() => {
-        async function formatRiskAssessmentSchedules(){
+        async function formatRiskAssessmentSchedules() {
             const items = {};
-            for (const schedule of riskAssessmentScheduleList){
-                items[schedule["dueDate"].slice(0, 10)] = [{name : schedule["title"], id: schedule["id"]}];
+            for (const schedule of riskAssessmentScheduleList) {
+                items[schedule.dueDate.slice(0, 10)] = [
+                    { name: schedule.title, id: schedule.id },
+                ];
             }
             setItemsListForAgenda(items);
         }
@@ -63,33 +95,27 @@ const CalendarScreen = ({navigation}) => {
 
     const renderItem = (item) => {
         return (
-        <TouchableOpacity style={{marginRight: 10, marginTop: 17}}>
-            <Card>
-                <Card.Content>
-                    <View 
-                        style={{
-                            flexDirection: 'row',
-                            justifyContent: 'space-between',
-                            alignItems: 'center'
-                        }}>
-                        <Text>{item.name}</Text>
-                    </View>
-                </Card.Content>
-            </Card>
-        </TouchableOpacity>
+            <TouchableOpacity style={styles.agendaItemContainer}>
+                <Card>
+                    <Card.Content>
+                        <View style={styles.agendaItemContentContainer}>
+                            <Text>{item.name}</Text>
+                        </View>
+                    </Card.Content>
+                </Card>
+            </TouchableOpacity>
         );
     };
 
     return (
-        <View style={{flex: 1}}>
+        <View style={styles.container}>
             <Agenda
-            items={itemsList}
-            selected={today}
-            renderItem={renderItem}
+                items={itemsList}
+                selected={today}
+                renderItem={renderItem}
             />
         </View>
     );
-
 };
 
 export default CalendarScreen;
